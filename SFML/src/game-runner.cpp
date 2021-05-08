05d18e3995b2e46305a309a6c933611d0505d877
rswc <rshoryti@protonmail.com>
2021-05-08 23:25:55 +0200

    Add move legality check

--- SFML/src/game-runner.cpp
+++ SFML/src/game-runner.cpp
@@ -1,161 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
+#include "game-runner.h"
+#include "board.h"
 
+extern Board board;
 
-
-enum PieceType {
-	pawn,
-	rook,
-	knight,
-	bishop,
-	king
-};
-
-enum PieceColor {
-	black,
-	white
-};
-
-struct Piece {
-	PieceType piece_type; 
-	PieceColor player_color;
-	int x = -1; //cords on board like A5, but translated properly
-	int y = -1;
-};
-
-
-std::vector< std::vector<int> > board(7, std::vector<int>(4));
-
-std::vector<Piece> alivePieces;
-
-int initBoard()
+bool isLegal(const Piece piece, int x, int y)
 {
-	struct Piece king;
+	if (x == piece.x && y == piece.y)
+		return false;
+
+	const Piece* other = bd_getPieceAtCords(x, y);
+
+	// For now, deny occupied squares
+	if (other)
+	{
+		return false;
+	}
+
+	int xs = (piece.x < x) ? 1 : -1, ys = (piece.y < y) ? 1 : -1;
+	int dx = abs(x - piece.x), dy = abs(y - piece.y);
+	int fwds = 2 * piece.player_color - 1;
+
+	switch (piece.type)
+	{
+	case king:
+		return abs(x - piece.x) <= 1 && abs(y - piece.y) <= 1;
+
+	case rook:
+		if (x == piece.x)
+		{
+			for (int i = piece.y + ys; i != y; i += ys)
+				if (board.board[x][i])
+						return false;
+		}
+		else if (y == piece.y)
+		{
+			for (int i = piece.x + xs; i != x; i += xs)
+				if (board.board[i][y])
+						return false;
+		}
+		else
+			return false;
+		break;
 	
+	case bishop:
+		if (abs(x - piece.x) != abs(y - piece.y))
+			return false;
 
-	for (size_t i = 0; i < 7; i++)
-	{
-		for (size_t j = 0; j < 4; j++)
+		for (int i = piece.x + xs, j = piece.y + ys; i < x; i += xs, j += ys)
+			if (board.board[i][j])
+				return false;
+		break;
+	
+	case knight:
+		return dx && dy && dx + dy == 3;
+	
+	case pawn:
+		if (x == piece.x && !board.board[x][y])
 		{
-			std::cout << board[i][j] << " " ;
+			if (y == piece.y + fwds)
+				return true;
+			
+			if (y == piece.y + 2 * fwds)
+				return !piece.hasMoved;
 		}
-		std::cout << std::endl;
-	}
-
-
-	//Add pawns
-	for (int i = 0; i < 4; i++)
-	{
-
-		struct Piece piece;
-		piece.piece_type = PieceType::pawn;
-		piece.player_color = PieceColor::white;
-		piece.x = i;
-		piece.y = 1;
-		alivePieces.push_back(piece);
-		piece.player_color = PieceColor::black;
-		piece.y = 5;
-		alivePieces.push_back(piece);
-
-		
-		switch (i)
-		{
-			case 0:
-			{
-				piece.piece_type = PieceType::rook;
-				piece.player_color = PieceColor::black;
-				piece.y = 6;
-				alivePieces.push_back(piece);
-				piece.piece_type = PieceType::king;
-				piece.player_color = PieceColor::white;
-				piece.y = 0;
-				alivePieces.push_back(piece);
-				break;
-			}
-			case 1:
-			{
-				piece.piece_type = PieceType::knight;
-				piece.player_color = PieceColor::black;
-				piece.y = 6;
-				alivePieces.push_back(piece);
-				piece.piece_type = PieceType::bishop;
-				piece.player_color = PieceColor::white;
-				piece.y = 0;
-				alivePieces.push_back(piece);
-				break;
-			}
-			case 2:
-			{
-				piece.piece_type = PieceType::bishop;
-				piece.player_color = PieceColor::black;
-				piece.y = 6;
-				alivePieces.push_back(piece);
-				piece.piece_type = PieceType::knight;
-				piece.player_color = PieceColor::white;
-				piece.y = 0;
-				alivePieces.push_back(piece);
-				break;
-			}
-			case 3:
-			{
-				piece.piece_type = PieceType::king;
-				piece.player_color = PieceColor::black;
-				piece.y = 6;
-				alivePieces.push_back(piece);
-				piece.piece_type = PieceType::rook;
-				piece.player_color = PieceColor::white;
-				piece.y = 0;
-				alivePieces.push_back(piece);
-				break;
-			}
-
-		}
-
-	}
-
-
-	//Print alive pieces to debug
-	for (int i = 0; i < alivePieces.size(); i++)
-	{
-		std::cout << alivePieces[i].player_color << std::endl;
+		return y == piece.y + fwds && dx == 1 && other;
 	}
 	
-	return 1;
-}
-
-
-/// <summary>
-/// Get piece at x,y. Return found struct.
-/// </summary>
-struct Piece getPieceAtCords(int x, int y)
-{
-	for (int i = 0; i < alivePieces.size(); i++)
-	{
-		if (alivePieces[i].x == x && alivePieces[i].y == y)
-		{
-			return alivePieces[i];
-		}
-	}
-	struct Piece found;
-	return found;
-}
-
-
- std::vector<Piece> &getPiecesOnBoard()
-{
-	return alivePieces;
-}
-
-
-int getPieceIndex(int x, int y)
-{
-	for (int i = 0; i < alivePieces.size(); i++)
-	{
-		if (alivePieces[i].x == x && alivePieces[i].y == y)
-		{
-			return i;
-		}
-	}
-	
-	return -1;
+	return true;
 }
